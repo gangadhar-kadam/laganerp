41aca1bc43282a0e2616ba348b348802d373aec8
gangadhar kadam <gangadhar.k@gmail.com>
2014-09-15 20:36:02 +0530

    for push upstream

--- erpnext/setup/doctype/site_master/site_master.py
+++ erpnext/setup/doctype/site_master/site_master.py
@@ -16,4 +16,43 @@ from distutils.spawn import find_executable
 
 class SiteMaster(Document):
 	def on_update(self):
-		pass
+		   frappe.errprint("creation site")
+		   res=frappe.db.sql("""select name from `tabSite Master` where flag=0 limit 1 """)
+		   frappe.errprint(res)
+		   if res:
+			sites=''
+			sites = frappe.db.sql("""select sites from  `tabUser` where name='administrator'""")
+			#print sites
+			#auto_commit = not from_test
+			ste=res[0][0]
+			from frappe.utils import cstr  
+			import os
+			import sys
+			import subprocess
+			import getpass
+			import logging
+			import json
+			from distutils.spawn import find_executable
+			cwd= '/home/gangadhar/workspace/smarttailor/frappe-bench/'
+		        cmd='bench new-site '+ste
+			frappe.errprint("created new site")
+			sites=cstr(sites[0][0])+' '+ste
+			#print sites
+			frappe.db.sql("update `tabUser` set sites= %s where name='administrator'",sites)
+		    	try:
+				subprocess.check_call(cmd, cwd=cwd, shell=True)
+			except subprocess.CalledProcessError, e:
+				print "Error:", e.output
+				raise
+		    	cmd='bench frappe --install_app erpnext '+ste              
+		    	try:
+				subprocess.check_call(cmd, cwd=cwd, shell=True)
+			except subprocess.CalledProcessError, e:
+				print "Error:", e.output
+				raise
+		    	cmd='bench frappe --install_app shopping_cart '+ste             
+		    	try:
+				subprocess.check_call(cmd, cwd=cwd, shell=True)
+			except subprocess.CalledProcessError, e:
+				print "Error:", e.output
+				raise
